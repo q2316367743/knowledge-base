8379db1a8a0f9bf2c9871257e6f548b8cde42463
落雨不悔 <m17762618644@163.com>
2023-10-14 01:04:11 +0800

    [todo]详情编辑器改回来，但是暴露editor.js

---
+++ src/components/editor-js/index.vue
@@ -0,0 +1,71 @@
+<template>
+    <div id="editor-js-editor"></div>
+</template>
+<script lang="ts" setup>
+import './index.less';
+import EditorJS from '@editorjs/editorjs';
+// @ts-ignore
+import Header from '@editorjs/header/dist/bundle.js';
+// @ts-ignore
+import List from '@editorjs/list/dist/bundle.js';
+// @ts-ignore
+import Table from '@editorjs/table/dist/table.js';
+import {onMounted, onUnmounted, PropType, watch} from "vue";
+
+const props = defineProps({
+    modelValue: Object as PropType<any>,
+});
+const emits = defineEmits(['update:modelValue']);
+
+let editor: EditorJS | null = null;
+
+onMounted(() => {
+    let data = props.modelValue;
+    if (editor) {
+        if (!data['version']) {
+            data  = {
+                "time": new Date().getTime(),
+                "blocks": [],
+                "version": "2.29.0-rc.1"
+            };
+        }
+    }
+    editor = new EditorJS({
+        holder: 'todo-editor—wrapper',
+        /**
+         * Available Tools list.
+         * Pass Tool's class or Settings object for each Tool you want to use
+         */
+        tools: {
+            header: {
+                class: Header,
+                inlineToolbar: true
+            },
+            list: {
+                class: List,
+                inlineToolbar: true
+            },
+            table: {
+                class: Table,
+            }
+        },
+        data: data,
+        /**
+         * onChange callback
+         */
+        onChange: (api) => {
+            api.saver.save().then(data => emits('update:modelValue', data))
+        }
+    });
+});
+
+
+onUnmounted(() => {
+    if (editor) {
+        editor.destroy();
+    }
+});
+</script>
+<style scoped>
+
+</style>
--- src/utils/utools/DbStorageUtil.ts
+++ src/utils/utools/DbStorageUtil.ts
@@ -1,4 +1,4 @@
-import { useAuthStore } from "@/store/components/AuthStore";
+import {useAuthStore} from "@/store/components/AuthStore";
 import {toRaw} from "vue";
 import {clone} from "xe-utils";
 
@@ -67,7 +67,7 @@ export async function saveListByAsync<T>(key: string, records: Array<T>, rev?: s
             // 查询后更新
             const res = await useAuthStore().authDriver.get(key);
             return await saveListByAsync(key, records, res ? res._rev : undefined);
-        }else if (res.message === 'An object could not be cloned.') {
+        } else if (res.message === 'An object could not be cloned.') {
             return await saveListByAsync(key, clone(records, true), rev);
         }
         console.log(res)
@@ -104,24 +104,35 @@ export async function getFromOneByAsync<T extends Record<string, any>>(key: stri
  * @param err 错误处理函数
  */
 export async function saveOneByAsync<T>(key: string, value: T, rev?: string, err?: (e: Error) => void): Promise<undefined | string> {
-    const res = await useAuthStore().authDriver.put({
-        _id: key,
-        _rev: rev,
-        value: toRaw(value)
-    });
-    if (res.error) {
-        if (res.message === "Document update conflict") {
-            // 查询后更新
-            const res = await useAuthStore().authDriver.get(key);
-            return await saveOneByAsync(key, value, res ? res._rev : undefined);
+    try {
+        const res = await useAuthStore().authDriver.put({
+            _id: key,
+            _rev: rev,
+            value: toRaw(value)
+        });
+        if (res.error) {
+            if (res.message === "Document update conflict") {
+                // 查询后更新
+                const res = await useAuthStore().authDriver.get(key);
+                return await saveOneByAsync(key, value, res ? res._rev : undefined);
+            }
+            if (err) {
+                err(new Error(res.message));
+            } else {
+                return Promise.reject(res.message);
+            }
         }
-        if (err) {
-            err(new Error(res.message));
-        }else {
-            return Promise.reject(res.message);
+        return Promise.resolve(res.rev);
+    } catch (e: any) {
+        console.log(typeof e.message)
+        console.log(e.message)
+        if (e.message === "An object could not be cloned.") {
+            // 查询后更新
+            return await saveOneByAsync(key, clone(value, true), rev);
+        } else {
+            return Promise.reject(e);
         }
     }
-    return Promise.resolve(res.rev);
 }
 
 /**
