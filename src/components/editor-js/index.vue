589616e93d83bdb27b859016662f4537b4303e06
落雨不悔 <m17762618644@163.com>
2023-10-15 00:28:17 +0800

    [编辑器]富文本编辑器：增加宽度检测

--- src/components/editor-js/index.vue
+++ src/components/editor-js/index.vue
@@ -2,7 +2,7 @@
     <div id="editor-js-editor"></div>
 </template>
 <script lang="ts" setup>
-import {onMounted, onUnmounted, PropType, watch} from "vue";
+import {onMounted, onUnmounted, PropType, watch, watchEffect} from "vue";
 import './index.less';
 import EditorJS from '@editorjs/editorjs';
 // 组件
@@ -22,6 +22,8 @@ import Raw from '@editorjs/raw/dist/bundle.js';
 import Underline from '@editorjs/underline/dist/bundle.js';
 import Warning from '@editorjs/warning/dist/bundle.js';
 import {UtoolsImage} from './plugins/UtoolsImage';
+import {useWindowSize} from "@vueuse/core";
+import NotificationUtil from "@/utils/NotificationUtil";
 
 const props = defineProps({
     modelValue: Object as PropType<any>,
@@ -32,6 +34,8 @@ const props = defineProps({
     }
 });
 const emits = defineEmits(['update:modelValue']);
+
+const size = useWindowSize();
 
 let editor: EditorJS | null = null;
 
@@ -116,6 +120,19 @@ onUnmounted(() => {
         }
     }
 });
+
+let showNotification = false;
+handleWidth(size.width.value);
+watch(() => size.width.value, value => handleWidth(value));
+function handleWidth(width:number) {
+    if (showNotification) {
+        return;
+    }
+    if (width < 1080) {
+        NotificationUtil.warning("检测到宽度小于1080px，可能造成编辑器显示不全");
+        showNotification = true;
+    }
+}
 </script>
 <style scoped>
 
