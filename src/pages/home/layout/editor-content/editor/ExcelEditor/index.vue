35210e58e453998ca089991aca1f1ba15f2f7093
落雨不悔 <m17762618644@163.com>
2024-03-03 22:12:50 +0800

    修复多个标签页问题

--- src/pages/home/layout/editor-content/editor/ExcelEditor/index.vue
+++ src/pages/home/layout/editor-content/editor/ExcelEditor/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <div id="excel-editor">表格</div>
+    <div :id="editorId" class="excel-editor">表格</div>
 </template>
 <script lang="ts" setup>
 import "@univerjs/design/lib/index.css";
@@ -18,7 +18,7 @@ import {UniverSheetsPlugin} from "@univerjs/sheets";
 import {UniverSheetsFormulaPlugin} from "@univerjs/sheets-formula";
 import {UniverSheetsUIPlugin} from "@univerjs/sheets-ui";
 import {UniverUIPlugin} from "@univerjs/ui";
-import {onMounted, onUnmounted, PropType} from "vue";
+import {onMounted, onUnmounted, PropType, ref} from "vue";
 import { useSaveContentEvent} from "@/store/components/HomeEditorStore";
 import type {Workbook} from "@univerjs/core/lib/types/sheets/workbook";
 
@@ -35,6 +35,10 @@ const props = defineProps({
 const emits = defineEmits(['update:modelValue']);
 
 
+const now = new Date().getTime();
+const editorId = ref('excel-editor-' + now);
+
+
 const univer = new Univer({
     theme: defaultTheme,
 });
@@ -47,7 +51,7 @@ onMounted(() => {
     univer.registerPlugin(UniverRenderEnginePlugin);
     univer.registerPlugin(UniverFormulaEnginePlugin);
     univer.registerPlugin(UniverUIPlugin, {
-        container: "excel-editor",
+        container: editorId.value,
         header: true,
         toolbar: true,
         footer: true,
@@ -85,7 +89,7 @@ function onSave() {
 
 </script>
 <style>
-#excel-editor {
+.excel-editor {
     width: 100%;
     height: 100%;
 }
