a956da80d5b821924387b15c2a099a1c3a2dc165
落雨不悔 <m17762618644@163.com>
2024-03-13 10:12:20 +0800

    待办：置顶

--- src/pages/todo/components/common/CardTodoItem.vue
+++ src/pages/todo/components/common/CardTodoItem.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="card-todo-item" :class="{deleted: index.status !== TodoItemStatus.TODO, top: index.top}"
+    <div class="card-todo-item" :class="{deleted: index.status !== TodoItemStatus.TODO}"
          :style="style"
          @click="_openTodoItemInfo()">
         <a-typography-paragraph style="margin-top: 9px" :ellipsis="ellipsis">
@@ -13,6 +13,11 @@
                 {{ start }}{{ end ? ' · ' + end : '' }}
             </a-tag>
         </div>
+        <a-tooltip content="置顶" v-if="index.top">
+            <div class="top">
+                <icon-arrow-up />
+            </div>
+        </a-tooltip>
     </div>
 </template>
 <script lang="ts" setup>
@@ -107,6 +112,7 @@ if (props.attr && index.value) {
     box-shadow: 0 -2px 5px rgba(0, 0, 0, 0.1);
     cursor: pointer;
     background-color: var(--color-fill-2);
+    position: relative;
 
     &.deleted {
         color: var(--color-text-4);
@@ -115,6 +121,17 @@ if (props.attr && index.value) {
         .arco-typography {
             color: var(--color-text-4);
         }
+    }
+    .top {
+        position: absolute;
+        top: 0;
+        right: 0;
+        background-color: rgb(var(--orange-6));
+        color: var(--color-fill-2);
+        clip-path: polygon(0 0, 100% 100%,100% 0);
+        width: 25px;
+        height: 25px;
+        text-align: right;
     }
 }
 </style>
