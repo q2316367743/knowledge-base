3a3579659c3e50799df50cd9db1dc35d19600ff1
落雨不悔 <m17762618644@163.com>
2024-12-23 23:01:31 +0800

    refactor(more/attachment):重构附件页面布局和功能
    - 重新设计了附件展示方式，使用卡片布局替代原有的滑动展示
    - 增加了复制 Markdown 链接和 HTML链接的功能
    - 优化了删除附件的逻辑，现在可以正确更新附件列表
    - 添加了无限滚动预览功能
    -调整了样式，使界面更加美观和现代化

--- src/pages/more/attachment/index.vue
+++ src/pages/more/attachment/index.vue
@@ -1,20 +1,28 @@
 <template>
-    <div class="more-attachment">
-        <div class="side" ref="side">
-            <div v-for="attachment in attachments" :key="attachment" class="item"
-                 :style="{backgroundImage: `url('${renderAttachmentUrl(attachment)}')`}" :data-id="attachment"/>
-        </div>
-        <div class="buttons">
-            <a-button :disabled="attachments.length === 0" type="primary" @click="pre()">上一张</a-button>
-            <a-button :disabled="attachments.length === 0" type="primary" @click="copy()" style="margin: 0 7px ;">
-                复制链接
-            </a-button>
-            <a-button :disabled="attachments.length === 0" type="primary" @click="remove()" status="danger"
-                      style="margin: 0 7px 0 0;">删除图片
-            </a-button>
-            <a-button :disabled="attachments.length === 0" type="primary" @click="next()">下一张</a-button>
-        </div>
-    </div>
+  <div class="more-attachment">
+    <a-image-preview-group infinite>
+      <a-space wrap>
+        <a-card :style="{ width: '351px' }" v-for="attachment in attachments" :key="attachment">
+          <template #actions>
+            <a-button size="mini" type="text" @click="copyMarkdown(attachment)">复制markdown链接</a-button>
+            <a-button size="mini" type="text" @click="copyHtml(attachment)">复制html链接</a-button>
+            <a-button size="mini" type="text" status="danger" @click="remove(attachment)">删除</a-button>
+          </template>
+          <template #cover>
+            <div style="display: flex;justify-content: center;align-items: center">
+              <a-image :src="renderAttachmentUrl(attachment)" height="120px"/>
+            </div>
+          </template>
+          <a-card-meta>
+            <template #title>
+              <a-typography-text>{{ attachment }}</a-typography-text>
+            </template>
+          </a-card-meta>
+        </a-card>
+      </a-space>
+    </a-image-preview-group>
+    <a-back-top target-container=".more-attachment" />
+  </div>
 </template>
 <script lang="ts" setup>
 import {ref} from "vue";
@@ -27,127 +35,44 @@ import {renderAttachmentUrl} from "@/plugin/server";
 const attachments = ref(new Array<string>());
 
 function fetchAttachment() {
-    attachments.value = [];
-    listRecordByAsync<any>(LocalNameEnum.ARTICLE_ATTACHMENT)
-        .then(items => {
-            items.forEach(item => attachments.value.push(item.id))
-        })
+  attachments.value = [];
+  listRecordByAsync<any>(LocalNameEnum.ARTICLE_ATTACHMENT)
+    .then(items => {
+      items.forEach(item => attachments.value.push(item.id))
+    })
 }
 
 fetchAttachment();
 
-// 动画处理
-const side = ref<HTMLDivElement | null>(null);
-let openClick = true;
-
-function next() {
-    if (openClick) {
-        openClick = false;
-        const items = document.querySelectorAll(".more-attachment .item");
-        if (side.value && items.length > 0) {
-            side.value.append(items[0]);
-            setTimeout(() => openClick = true, 1000);
-        } else {
-            openClick = true
-        }
-    }
+function copyMarkdown(item: string) {
+  utools.copyText(`![](${item})`);
+  MessageUtil.success("成功复制到剪切板");
 }
 
-function pre() {
-    if (openClick) {
-        openClick = false;
-        const items = document.querySelectorAll(".more-attachment .item");
-        if (side.value && items.length > 0) {
-            side.value.prepend(items[items.length - 1]);
-            setTimeout(() => openClick = true, 1000);
-        } else {
-            openClick = true
-        }
-
-    }
+function copyHtml(item: string) {
+  utools.copyText(`![](${renderAttachmentUrl(item)})`);
+  MessageUtil.success("成功复制到剪切板");
 }
 
-function copy() {
-    const item = document.querySelector(".more-attachment .item:nth-child(1)")!;
-    let id = item?.attributes.getNamedItem('data-id')?.value;
-    utools.copyText(`![](${id})`);
-    MessageUtil.success("成功复制到剪切板");
-}
 
-function remove() {
-    MessageBoxUtil.confirm("删除此附件后，使用的文章将无法展示附件", "删除确认", {
-        confirmButtonText: "删除",
-        cancelButtonText: "取消"
-    }).then(() => {
-        const item = document.querySelector(".more-attachment .item:nth-child(1)")!;
-        removeOneByAsync(item?.attributes.getNamedItem('data-id')?.value!)
-            .then(() => {
-                if (side.value) {
-                    side.value.removeChild(item);
-                    MessageUtil.success("删除成功");
-                }
-            }).catch(e => MessageUtil.error("删除失败", e));
-    })
+function remove(item: string) {
+  MessageBoxUtil.confirm("删除此附件后，使用的文章将无法展示附件", "删除确认", {
+    confirmButtonText: "删除",
+    cancelButtonText: "取消"
+  }).then(() => {
+    removeOneByAsync(item)
+      .then(() => {
+        fetchAttachment();
+        MessageUtil.success("删除成功");
+      }).catch(e => MessageUtil.error("删除失败", e));
+  })
 }
 
 </script>
 <style scoped lang="less">
 .more-attachment {
-    position: relative;
-    width: 100%;
-    height: 100%;
-    background: linear-gradient(200deg, pink, #918ef9);
-    overflow: hidden;
-
-    .item {
-        width: 240px;
-        height: 160px;
-        position: absolute;
-        top: 50%;
-        left: 0;
-        transform: translateY(-50%);
-        border-radius: 2px;
-        background-size: cover;
-        background-color: pink;
-        background-position: center;
-        transition: 1s;
-        box-shadow: 0 30px 50px #505050;
-
-        &:nth-child(1) {
-            left: 0;
-            top: 0;
-            width: 100%;
-            height: 100%;
-            transform: translateY(0);
-            box-shadow: none;
-            border-radius: 0;
-        }
-
-        &:nth-child(2) {
-            left: 70%;
-        }
-
-        &:nth-child(3) {
-            left: calc(70% + 250px);
-        }
-
-        &:nth-child(4) {
-            left: calc(70% + 500px);
-        }
-
-        &:nth-child(n+5) {
-            left: calc(70% + 750px);
-            opacity: 0;
-        }
-    }
-
-    .buttons {
-        width: 100%;
-        position: absolute;
-        bottom: 50px;
-        text-align: center;
-        display: flex;
-        justify-content: center;
-    }
+  padding: 8px 8px 0;
+  height: calc(100vh - 16px);
+  overflow: auto;
 }
 </style>
